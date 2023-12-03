d9a5989b393689e1fe6087caff13ef32a2748106
Atris <vacekj@outlook.com>
2023-12-03 23:14:35 +0100

    feat: docs

---
+++ docs/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4uuVipJrShRslJyzSwwMjM0CijKLyhW0lEqSCzJAIqWVBakFusjy+lllOTmABVkZ+alKFkZGViaG5oa1erAzQlILcrNLAnOTM9LLCktSkU3Ck2akGkghRAtWB2GJk3ItGK4coRBaaV5ySWZ+XnF+ghZVHPMTLAa4ZKYid8UoAI8BpUWp+J2ClwSw4BYAHX6iGqyAQAA"
